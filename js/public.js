ffc33db36737d91369c44d431aa3dd302e77ba8a
dageqin <hdgeqin@126.com>
2018-11-30 10:26:01 +0800

    first commit

---
+++ js/public.js
@@ -0,0 +1,12 @@
++ function() {
+    remLayout();
+    function remLayout() {
+        var w = document.documentElement.clientWidth;
+        w = w > 768 ? 768 : w;
+        w = w <= 320 ? 320 : w;
+        document.documentElement.style.fontSize = w / 7.5 + 'px';
+    }
+    window.addEventListener('resize', function() {
+        remLayout();
+    }, false);
+}();
