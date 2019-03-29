79df076163e42db6dc52c509f115eed410399a92
dageqin <hdgeqin@126.com>
2019-03-29 17:56:34 +0800

    modify more submit

--- js/login.js
+++ js/login.js
@@ -60,37 +60,51 @@ $getCode.on('tap', function () {
 });
 
 /*前往帮忙*/
+var isSubmit = false;
 $submit.on('tap', function (e) {
     e.preventDefault();//阻止form表单默认提交
     var reqData = window.location.href.queryURLParameter();
     var helpUserId = reqData.helpUserId;
     $("#helpUserId").val(helpUserId);
-    console.log($("#formInfo").serialize());
-    $.ajax({
-        type: "post",
-        url: "/h5/h5Regester",
-        data: $("#formInfo").serialize(),
-        dataType: 'json',
-        contentType: 'application/json',
-        success: function (res) {
-            console.log(res);
-            if(!res) return;
-            if(res.code == 600){
-                alert('验证码错误！');
-                return;
-            }else if(res.code == 100){
-                alert('验证码已过期！');
-                return;
-            }else {
-                alert(res.desc);
+    $submit.css('background', '#e6e6e6');
+    if(!isSubmit){
+        isSubmit = true;
+        $.ajax({
+            type: "post",
+            url: "/h5/h5Regester",
+            data: $("#formInfo").serialize(),
+            dataType: 'json',
+            contentType: 'application/json',
+            success: function (res) {
+                console.log(res);
+                if(!res) return;
+                if(res.code == 600){
+                    alert('验证码错误！');
+                    isSubmit = false;
+                    $submit.css('background', '#FCD204');
+                    return;
+                }else if(res.code == 100){
+                    alert('验证码已过期！');
+                    isSubmit = false;
+                    $submit.css('background', '#FCD204');
+                    return;
+                }else {
+                    alert(res.desc);
+                }
+                //验证成功，启动app，否则跳到下载页面
+                window.location.href = './download.html';
+            },
+            error: function (err) {
+                alert(err);
+                isSubmit = false;
+                $submit.css('background', '#FCD204');
             }
-            //验证成功，启动app，否则跳到下载页面
-            window.location.href = './download.html';
-        },
-        error: function (err) {
-            alert(err);
-        }
-    })
+        })
+    }else{
+        $submit.css('background', '#e6e6e6');
+        return false;
+    }
+
 });
 
 
