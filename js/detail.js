bb0c6bcb9e2d4c923c518c34fceb54aabf7c7c93
Kikige <hdgeqin@126.com>
2019-06-14 17:23:59 +0800

    update detail

--- js/detail.js
+++ js/detail.js
@@ -16,8 +16,8 @@ var res = {
             "https://bgm-1258164185.cos.ap-shanghai.myqcloud.com/bgm/1547552248472145767",
             "https://bgm-1258164185.cos.ap-shanghai.myqcloud.com/bgm/1547552248472145767",
             "https://bgm-1258164185.cos.ap-shanghai.myqcloud.com/bgm/1547552248472145767"],
-        "personalLabel": ["靠谱","随性"],
-        "publishContent": "张宇涛，男，9岁，身高约123cm，于2018年12月10日在河南省安阳市都里镇李珍村失踪。孩子出生在李珍村，智力有些问题，未上学，平时不爱说话。2018年12月10日下午3点左右，孩子独自出门去外面玩，到了吃晚饭时间（大概下午5-6点左右）家人找孩子吃饭时不见了孩子，在村里和附近都找遍了也没找到\n\n孩子失踪时身穿蓝色棉袄，浅蓝色裤子，脚穿一双棉鞋。现在家人万分着急！\n\n\n宝贝快回来,你的家人都不会怪你。有孩子任何消息的知情者可以在线联系我们提供线索，或者拨打宝贝回家求助站电话：0435-3338090（吉林通化）\n\n如果有陌生人收留他。有什么要求也可以直接联系我们。只要孩子安全回来，一切可以既往不咎。\n\n你的随手一转，也许就是这家人团圆的希望\n恳请大家帮忙转发一下，让这家人在新的一年能够团圆过年。",
+        "personalLabel": ["靠谱", "随性"],
+        "publishContent": "张宇涛，男，9岁，身高约123cm，于家人找孩子吃饭时不见了孩子，在村里和附近都找遍了也没找到\n\n孩子失踪时身穿蓝色棉袄，浅蓝色裤子，脚穿一双棉鞋。现在家人万分着急！\n\n\n宝贝快回来,你的家人都不会怪你。",
         "industry": "技术员",
         "userName": "宝贝回家",
         "userId": "26b374079fa1400e8377726883a99011",
@@ -33,32 +33,32 @@ var res = {
         "pstate": false, //是否公益
         "followSize": 10, //公益次数
         "itisRedMoney": true, //显示红包
-        "itisPais":1,//是否有偿 1是 2否
-        "listSize:":30,
-        "list":[{
-            usersHead:'./images/girl.jpg',
-            usersName:'kiki',
-            money:0.02
-        },{
-            usersHead:'./images/girl.jpg',
-            usersName:'lili',
-            money:0.01
-        },{
-            usersHead:'./images/girl.jpg',
-            usersName:'kiki',
-            money:0.02
-        },{
-            usersHead:'./images/girl.jpg',
-            usersName:'lili',
-            money:0.01
-        },{
-            usersHead:'./images/girl.jpg',
-            usersName:'kiki',
-            money:0.02
-        },{
-            usersHead:'./images/girl.jpg',
-            usersName:'lili',
-            money:0.01
+        "itisPais": 1,//是否有偿 1是 2否
+        "listSize:": 30,
+        "list": [{
+            usersHead: './images/girl.jpg',
+            usersName: 'kiki',
+            money: 0.02
+        }, {
+            usersHead: './images/girl.jpg',
+            usersName: 'lili',
+            money: 0.01
+        }, {
+            usersHead: './images/girl.jpg',
+            usersName: 'kiki',
+            money: 0.02
+        }, {
+            usersHead: './images/girl.jpg',
+            usersName: 'lili',
+            money: 0.01
+        }, {
+            usersHead: './images/girl.jpg',
+            usersName: 'kiki',
+            money: 0.02
+        }, {
+            usersHead: './images/girl.jpg',
+            usersName: 'lili',
+            money: 0.01
         }]
     }
 };
@@ -73,7 +73,7 @@ function renderDOM(res, helpId, publishId) {
     var listLen = dataList.list && dataList.list.length;
     var personLable = dataList.personalLabel;
     //多个个人标签显示一个 数组
-    if(personLable.length > 1){
+    if (personLable.length > 1) {
         dataList.personalLabel = dataList.personalLabel[0];
     }
     //对应的标题图标
@@ -121,13 +121,13 @@ function renderDOM(res, helpId, publishId) {
     var helpUserName = dataList.helpUserName;
     //名称取前三个
     //英文,数字,空格
-    if(/^[0-9A-Za-z]+(\s[0-9A-Za-z]+)*$/.test(helpUserName)){
-        if(helpUserName.length > 7){
+    if (/^[0-9A-Za-z]+(\s[0-9A-Za-z]+)*$/.test(helpUserName)) {
+        if (helpUserName.length > 7) {
             dataList.helpUserName = helpUserName.slice(0, 7);
         }
-    }else{
-        if(helpUserName.length > 3){
-            dataList.helpUserName = helpUserName.slice(0, 3);
+    } else {
+        if (helpUserName.length > 4) {
+            dataList.helpUserName = helpUserName.slice(0, 4) + '...';
         }
     }
 
@@ -142,11 +142,11 @@ function renderDOM(res, helpId, publishId) {
             dataList.gender = './images/nv.png';
     }
     //最多显示9张图
-    if(imgLen > 9){
+    if (imgLen > 9) {
         dataList.images.splice(9);
     }
     //最多显示4个人
-    if(listLen > 4){
+    if (listLen > 4) {
         dataList.list.splice(4);
     }
 
@@ -167,133 +167,129 @@ function renderDOM(res, helpId, publishId) {
     var bShow = true;
     var imgShow = true;
 
-    if(gender == 1){
-        $gender.css('background-color','#56d8da');
-    }else if(gender == 2){
-        $gender.css('background-color','#fb8e8e');
+    if (gender == 1) {
+        $gender.css('background-color', '#56d8da');
+    } else if (gender == 2) {
+        $gender.css('background-color', '#fb8e8e');
     }
     //显示有偿
-    if(isPais == 1){
+    if (isPais == 1) {
         $pay.show();
-    }else {
+    } else {
         $pay.hide();
     }
     //显示帮转红包
-    if(isRedbag){
+    if (isRedbag) {
         $redbag.show();
-    }else {
+    } else {
         $redbag.hide();
     }
     var contentHeight = $content.height();
-    console.log(contentHeight);
     $footer.css('top', contentHeight - 60);
     var originHeight = $contentImg.height();
-    if(imgLen == 0){
+    if (imgLen == 0) {
         $contentImg.height(0);
-    }else if(imgLen > 3){
+    } else if (imgLen > 3) {
         var num = Math.ceil(imgLen / 3);
-        if(num > 3){
+        if (num > 3) {
             num = 3;
         }
         $contentImg.height(originHeight * num);
     }
-
-    //控制全文显示
-    // if (contentLen <= 118) {
-    //     $showBtn.css('display', 'none');
-    // } else {
-    //     $showBtn.css('display', 'inline-block');
-    //     var showText = content.slice(0, 118);
-    //     //全文按钮的点击事件
-    //     $showBtn.on('tap', function () {
-    //         if (bShow) {
-    //             $contentDetail.css('overflow', 'auto');
-    //             $contentDetail.text(content);
-    //         } else {
-    //             $contentDetail.css('overflow', 'hidden');
-    //             $contentDetail.text(showText);
-    //         }
-    //         h = $content.height();
-    //         $footer.css('top', h - 70);
-    //         bShow = !bShow;
-    //         $showBtn.text(bShow ? '全文' : '收起');
-    //     });
-    // }
-
-    /*图片的展开、收起*/
-    // var $imageOpen = $('#imageOpen'); //展开图片按钮
-    // if (imgLen <= 3) {
-    //     $imageOpen.css('display', 'none');
-    // } else {
-    //     $imageOpen.css('display', 'inline-block');
-    //     var originHeight = $contentImg.height();
-    //     $imageOpen.on('tap', function () {
-    //         if (imgShow) {
-    //             $contentImg.css('overflow', 'auto');
-    //             var num = Math.ceil(imgLen / 3);
-    //             $contentImg.height(originHeight * num);
-    //         } else {
-    //             $contentImg.height(originHeight);
-    //             $contentImg.css('overflow', 'hidden');
-    //         }
-    //         var h = $content.height();
-    //         $footer.css('top', h - 70);
-    //         imgShow = !imgShow;
-    //         /*控制按钮箭头方向*/
-    //         if (imgShow) {
-    //             $imageOpen.css('transform', '');
-    //         } else {
-    //             $imageOpen.css('transform', 'rotate(180deg)');
-    //         }
-    //     })
-    // }
-
-    //删除无数据tag的标签
-    $('.tag').forEach(function (item) {
-        if (item.innerText == '') {
-            $('.header_person')[0].removeChild(item);
-        }
-    });
     //领取红包
     $("#redBtn").on('tap', function () {
         window.location = './redbag.html?helpUserName=' + helpUserName;
     });
     //立即帮他
-    $("#helpBtn").on('tap',function () {
-        window.location = './login.html?helpUserId=' + helpId + '&publishId='+publishId;
+    $("#helpBtn").on('tap', function () {
+        window.location = './login.html?helpUserId=' + helpId + '&publishId=' + publishId;
     });
     //点击图片放大
+    var imgSwiper = new Swiper('#imgWrap', {
+        initialSlide: 0,
+        centeredSlides: true,
+        loop: false,
+        slidesPerView: 1,
+        observer: true,
+        observeParents: true
+    });
     var imgsObj = $('#contentImg li img');//需要放大的图像
-    console.log(imgsObj);
-    if(imgsObj){
-        $.each(imgsObj,function(){
-            $(this).on('tap',function(){
-                var imgSrc = $(this).attr('src');
-                $("#bigImg").attr('src', imgSrc);
-                $('.showBigImg').css('display', 'block');
-                $('#showMain').css('display', 'none');
-                var h = $("#bigImg").css('height');
-                h = parseInt(h.slice(0,6));
-                var wH = document.body.clientHeight;
-                if(h >= wH){
-                    $("#bigImg").css('margin-top', 0);
-                }else{
-                    $("#bigImg").css('margin-top', (wH - h)/2);
-                }
-            });
-        })
+    if (imgsObj) {
+        for (var i = 0; i < imgsObj.length; i++) {
+            (function (index) {
+                //图片滑动
+                $(imgsObj[index]).on('tap', function () {
+                    var offsetHeight = document.body.clientHeight * 1.2;
+                    console.log(offsetHeight);
+                    imgSwiper.slideTo(index);
+                    imgSwiper.on('tap', function () {
+                        $("#showMain").scrollTop(offsetHeight);
+                        $('.showBigImg').css('display', 'none');
+                        $('#showMain').css('display', 'block');
+                    });
+                    setTimeout(function () {
+                        $('.showBigImg').css('display', 'block');
+                        $('#showMain').css('display', 'none');
+                    }, 300);
+                });
+            })(i);
+        }
     }
-    $('.showBigImg').on('tap', function(){
-        $('.showBigImg').css('display', 'none');
-        $('#showMain').css('display', 'block');
-    })
+    //图片缩放
+    var pageX, pageY, initX, initY, isTouch = false;
+    var start = [];
+    console.log($("#imgWrap"));
+    $("#imgWrap").on("touchstart", function (e) {
+        //手指按下时的手指所在的X，Y坐标  
+        pageX = e.targetTouches[0].pageX;
+        pageY = e.targetTouches[0].pageY;
+        //初始位置的X，Y 坐标  
+        initX = e.target.offsetLeft;
+        initY = e.target.offsetTop;
+
+        //记录初始 一组数据 作为缩放使用
+        if (e.touches.length >= 2) { //判断是否有两个点在屏幕上
+            start = e.touches; //得到第一组两个点
+        }
+
+        //表示手指已按下  
+        isTouch = true;
+    }, false);
+    //监听 touchmove 事件 手指 移动时 做的事情
+    $("#imgWrap").on("touchmove", function (e) {
+        e.preventDefault();
+        // 2 根 手指执行 目标元素放大操作
+        if (e.touches.length >= 2 && isTouch && e.scale < 2.5) {
+            //得到第二组两个点
+            var now = e.touches;
+            //得到缩放比例， getDistance 是勾股定理的一个方法
+            var scale = (getDistance(now[0], now[1]) / getDistance(start[0], start[1]));
+            // 对缩放 比例 取整
+            e.scale = scale.toFixed(2);
+            // 执行目标元素的缩放操作
+            e.target.style.transform = "scale(" + scale + ")";
+        }
+    }, false);
+    //监听 手指离开屏幕时 
+    $("#imgWrap").on("touchend", function (e) {
+        //将 isTouch 修改为false  表示 手指已经离开屏幕
+        if (isTouch) {
+            isTouch = false;
+        }
+    }, false);
+
+    //缩放 勾股定理方法
+    function getDistance(p1, p2) {
+        var x = p2.pageX - p1.pageX,
+            y = p2.pageY - p1.pageY;
+        return Math.sqrt((x * x) + (y * y));
+    }
 }
 
 
 //接口 调取数据
 $(function () {
     var reqData = window.location.href.queryURLParameter();
-    console.log(reqData);
     var url = '/h5/getH5PublishDetail';
     var data = {
         publishId: reqData.publishId,
@@ -307,7 +303,6 @@ $(function () {
         dataType: 'json',
         contentType: 'application/json',
         success: function (res) {
-            console.log(res);
             if (!res) return;
             if (res.code !== 200) {
                 alert(res.desc);
